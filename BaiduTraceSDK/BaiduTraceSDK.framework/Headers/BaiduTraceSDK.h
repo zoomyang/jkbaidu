2fbefd0896facfb73f23d2ec708a3faedc5a0344
Jarek Yang <373330163@qq.com>
2017-02-07 08:13:23 +0800

    提交鹰眼SDK

---
+++ BaiduTraceSDK/BaiduTraceSDK.framework/Headers/BaiduTraceSDK.h
@@ -0,0 +1,24 @@
+//
+//  BaiduTraceSDK.h
+//  BaiduTraceSDK
+//
+//  Created by Daniel Bey on 2016/12/15.
+//  Copyright © 2016年 Daniel Bey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaiduTraceSDK.
+FOUNDATION_EXPORT double BaiduTraceSDKVersionNumber;
+
+//! Project version string for BaiduTraceSDK.
+FOUNDATION_EXPORT const unsigned char BaiduTraceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaiduTraceSDK/PublicHeader.h>
+
+#import <BaiduTraceSDK/SqliteBriding.h>
+
+#ifndef __BaiduTraceSDK__Umbrella__
+#define __BaiduTraceSDK__Umbrella__
+#include "CppInterface.h"
+#endif
